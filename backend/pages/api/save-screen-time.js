5fda6dcf2c0dc0689808c693859b5b0709a53fed
kabimurugan <Contactksbalaji@gmail.com>
2026-04-01 16:09:42 +0530

    done

--- backend/pages/api/save-screen-time.js
+++ backend/pages/api/save-screen-time.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
     res.status(200).end();
     return;
   }
+  
 
   if (req.method !== 'POST') {
     res.status(405).json({ success: false, message: 'Method not allowed' });
