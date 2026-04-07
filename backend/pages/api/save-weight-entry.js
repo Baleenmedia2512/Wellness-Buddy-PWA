2aaac811cfa9619966697f3a201123b9566f6a1d
Baleenmedia2512 <Contactksbalaji@gmail.com>
2026-04-07 13:09:26 +0530

    done

--- backend/pages/api/save-weight-entry.js
+++ backend/pages/api/save-weight-entry.js
@@ -9,7 +9,7 @@ import { largeBodyConfig as config } from "../../utils/apiConfig.js";
 export { config };
 
 export default async function handler(req, res) {
-  // Set CORS headers for all requests
+  // Set CORS headers for all requests.
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
