0f46d27c42fe6b977c445d93c806837f7a9f106a
Adhithya <Contactksbalaji@gmail.com>
2026-04-20 12:43:31 +0530

    chore: add a blank line for improved code readability in get-time-windows.js

--- backend/pages/api/get-time-windows.js
+++ backend/pages/api/get-time-windows.js
@@ -1,5 +1,6 @@
 import { getTimeWindows } from '../../utils/disciplineCalculationsSupabase.js';
 
+
 export default async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
